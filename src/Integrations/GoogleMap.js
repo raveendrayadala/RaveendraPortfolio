ea8480c85feb854e6b3826c8d37b307ee416bfa9
Raveendra Yadala <raveendra.yadala@accenture.com>
2020-04-06 15:52:41 +0530

    Initial commit

---
+++ src/Integrations/GoogleMap.js
@@ -0,0 +1,15 @@
+import React , { Component } from 'react';
+
+class GooglePage extends Component {
+
+    render() {
+      return (
+        <div>
+       <h1>This is Google map page1</h1>
+      
+       </div>
+      )
+    }
+  }
+  export default GooglePage;
+  
--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import registerServiceWorker from './registerServiceWorker';
+import GooglePage from './Integrations/GoogleMap';
+import { BrowserRouter as Router , Switch , Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+  <Router>
+  <div>
+     <Switch>
+        <Route exact path="/" component={App} />
+        <Route exact path="/GoogleMap" component={GooglePage} />
+      </Switch>
+  </div>
+ </Router>
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(routing,document.getElementById('root'));
+registerServiceWorker();
